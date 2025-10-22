dc5200e431350e60824a0763f0cb495ab8393f2a
M6ct0night <efe20171999@gmail.com>
2025-10-22 18:58:47 +0100

    cevaplar ve id

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,23 +5,37 @@ public class Main {
         System.out.println("Hello world!");
     }
 
-    public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+    public static boolean shouldWakeUp(boolean isBarking, int hour) {
+        if (isBarking && (hour < 8 || hour > 20)) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
+
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+       return (firstAge >= 13 && firstAge <= 19) || (secondAge >= 13 && secondAge <= 19) || (thirdAge >= 13 && thirdAge <= 19);
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+     if(isSummer == true && (temp>25 && temp < 45)){return true;
+     }else if(isSummer == false && (temp>25 && temp < 35)){
+         return true;
+     }else { return false;
+     }
     }
 
     public static double area(double width, double height) {
-       
+       double result = width*height;
+       if(result > 0){return result;
+       }else{return -1;}
     }
 
     public static double area(double radius) {
-       
+        if (radius < 0) {
+            return -1;  // negatif değer gelirse -1 döndür
+        }
+        return radius * radius * Math.PI;
     }
 }
